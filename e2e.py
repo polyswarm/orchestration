d01ac9f33eeead86e54d36e58ca4ff784475e841
Nick Lang <nickl123@ufl.edu>
2018-07-23 16:00:01 -0700

    progress2

--- e2e.py
+++ e2e.py
@@ -13,7 +13,9 @@ init()
 ab_path = os.path.abspath(args.dir)
 PS_GITHUB_URL = 'https://github.com/polyswarm/'
 PS_REPOS = ["polyswarmd", "ambassador", "arbiter", "contracts", "priv-testnet", "microengine","orchestration"]
+ALL_REPOS = ["polyswarmd", "ambassador", "arbiter", "contracts", "priv-testnet", "microengine","orchestration"]
 stars='***********************************'
+
 
 print(Fore.MAGENTA+"your polyswarm repos lie in: " + ab_path)
 #enum files in specified dir, check em and pop what they got
@@ -26,26 +28,51 @@ for filename in os.listdir(ab_path):
 
 #if they don't have everything
 if (PS_REPOS):				
+
 	print('Detected some repos, but here\'s what you need:')
 	print(str(PS_REPOS)+Style.RESET_ALL)
 	sleep(2)
 	os.chdir(ab_path)
 	command = 'git clone -b master {0}{1}'.format(PS_GITHUB_URL,repo)
-	print(command)
+
 	for repo in PS_REPOS:
 		subprocess.run(command.split())
 		print(Fore.MAGENTA+"cloned master branch of " + repo)
-		print(stars+"\nbuilding image from cloned repo...\n"+stars+Style.RESET_ALL )
+		print(stars+"\nbuilding image from cloned repo...\n"+stars+Style.RESET_ALL)
 		sleep(2)
 		os.chdir(os.path.join(ab_path,repo))
 		command2 = 'docker build -t polyswarm/{0} -f docker/Dockerfile .'.format(repo)
 		subprocess.run(command2.split())
+#if they got everything
 else:
-	print(Fore.MAGENTA+"You have all of our repos in your specified directory,\
+	print(Fore.MAGENTA+"You have all of our necessary repos in your specified directory,\
  please make sure they're up to date :)")
+	sleep(3)
+#build remaining repos
+TO_BUILD = list(set(ALL_REPOS) - set(PS_REPOS))
+print("repos to build: "+str(TO_BUILD) + Style.RESET_ALL)
+for repo in TO_BUILD:
+	os.chdir(os.path.join(ab_path,repo))
+	cmd = 'docker build -t polyswarm/{0} -f docker/Dockerfile .'.format(repo)
+	subprocess.run(cmd.split())
+cleancmd = 'docker container prune -f && docker image prune -f'
+subprocess.run(cleancmd)
 
-
-print("beginning testing..." + Style.RESET_ALL)
+print(Fore.MAGENTA+"beginning testing..." + Style.RESET_ALL)
 sleep(1)
 composecmd = 'docker-compose -f {0}/dev.yml -f {0}/test.yml up'.format(os.path.join(ab_path,'orchestration'))
 subprocess.run(composecmd.split())
+
+projs_to_check = ["contracts","arbiter","ambassador","microengine"]
+
+for proj in projs_to_check:
+	id_cmd = 'docker-compose -f {1}/dev.yml -f {1}/test.yml ps -q {0}'.format(proj,os.path.join(ab_path,'orchestration'))
+	c_id = subprocess.check_output(id_cmd.split()).decode('ASCII')
+
+	ret_cmd = 'docker wait {0}'.format(c_id)
+	retval_proj = subprocess.check_output(ret_cmd.split()).decode('ASCII')
+	if retval_proj==str("0\n"):
+		print(Fore.MAGENTA+"testing "+Fore.GREEN+proj+Fore.MAGENTA+" success :)")
+	else:
+		print(Fore.MAGENTA+"testing "+Fore.GREEN+proj+Fore.RED+" failed :(")
+	print(Style.RESET_ALL)
